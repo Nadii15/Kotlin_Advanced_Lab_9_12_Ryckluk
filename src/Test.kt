03ac02f05f03ce432f2c4d03ddec7c80d13cbc00
Nadii15 <nadigar1506@mail.ru>
2026-01-19 16:08:21 +0300

    Add abstract class

--- src/Test.kt
+++ src/Test.kt
@@ -4,10 +4,46 @@ var age: Int = 18
             field = value
     }
 
-fun main(){
+fun main() {
     println(age)
     age = 45
     println(age)
     age = -345
     println(age)
-}
+
+    abstract class Human(val name: String) {
+        abstract var age: Int
+        abstract fun hello()
+
+
+    }
+
+    //    val denis :Human
+//    val pavel: Human = Human("Pravel")
+    class Person(name: String) : Human(name) {
+        override var age: Int = 1
+        override fun hello() {
+            println("My name is $name")
+            abstract class Figure {
+                abstract fun perimeter(): Float
+                abstract fun area(): Float
+            }
+
+            class Rectangle(val width: Float, val height: Float) : Figure() {
+                override fun perimeter(): Float {
+                    return 2 * (width + height)
+                }
+
+                override fun area(): Float {
+                    return width * height
+                }
+            }
+        }
+
+
+            }
+        }
+
+
+
+
