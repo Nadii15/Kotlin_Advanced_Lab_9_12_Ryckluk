56e8e74d93701bdda0a7858d92ad1d069741f717
Nadii15 <nadigar1506@mail.ru>
2026-01-21 08:59:30 +0300

    Add abstract modules, interfaces and first module implementations

---
+++ src/ResearchLab.kt
@@ -0,0 +1,26 @@
+class ResearchLab: OutpostModule("Исследовательская лаборатория") {
+    override fun performAction(manager: ResourceManager) {
+        var minerals = manager.get("Minerals")
+        if (minerals == null || minerals.amount < 30) {
+            println("Недостаточно минералов для исследования")
+            return
+        }else{
+            minerals.amount -= 30
+            println("")
+        }
+
+
+    }
+
+}
+fun main(){
+    val manager = ResourceManager()
+    manager.add(OutpostResource(1,"Minerals",120))
+    manager.add(OutpostResource(2, "Gas",40))
+    val generator = EnergyGenerator()
+    val lab = ResearchLab()
+    generator.performAction(manager)
+    lab.performAction(manager)
+    println()
+    manager.printAll()
+}
