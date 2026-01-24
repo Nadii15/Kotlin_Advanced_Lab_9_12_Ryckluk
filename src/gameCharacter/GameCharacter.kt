e770c1f921de26adeeb83acb1194b5af03357914
Aleckseev Grisha <grisha.zhivot@bk.ru>
2026-01-24 03:40:49 +0300

    "add character state system using sealed-classes"

---
+++ src/gameCharacter/GameCharacter.kt
@@ -0,0 +1,10 @@
+package gameCharacter
+
+class GameCharacter(val name: String) {
+    var state: CharacterState = CharacterState.Idle
+    private set
+
+    fun changeState(newState: CharacterState){
+        state = newState
+    }
+}
---
+++ src/gameCharacter/Main.kt
@@ -0,0 +1,21 @@
+package gameCharacter
+
+fun handleState(state: CharacterState){
+    when(state){
+        is CharacterState.Idle -> println("Персанаж бездействует")
+        is CharacterState.Running -> println("Персанаж бежит")
+        is CharacterState.Attacking -> println("Персанаж атакует с уроном ${state.damage}")
+        is CharacterState.Dead -> println("Персанаж погиб: ${state.cause}")
+    }
+}
+fun main(){
+    val character = GameCharacter("Герой")
+    character.changeState(CharacterState.Running)
+    handleState(character.state)
+
+    character.changeState(CharacterState.Attacking(42))
+    handleState(character.state)
+
+    character.changeState(CharacterState.Dead("Убит драконом"))
+    handleState(character.state)
+}
