920781705c33c40c0f3fc5f98697556f468e2d9d
Aleckseev Grisha <grisha.zhivot@bk.ru>
2026-01-25 17:59:36 +0300

    "add delegation example"

--- src/Maim.kt
+++ src/Maim.kt
@@ -1,44 +1,62 @@
-import modules.EnergyGenerator
-import modules.ModuleResult
-import modules.ResearchLab
-import resources.OutpostResource
-import resources.ResourceManager
+import example.counter
+
+//import example.InstantMessenger
+//import example.PhotoCamera
+//import modules.EnergyGenerator
+//import modules.ModuleResult
+//import modules.ResearchLab
+//import resources.OutpostResource
+//import resources.ResourceManager
+//fun main(){
+//    val manager = ResourceManager()
+//    val minerals = OutpostResource(1,"Minerals",300)
+//    val gas = OutpostResource(2,"Gas",100)
+//    val bonus = minerals.copy(amount = minerals.amount + 50)
+//    println("Копия минералов с бонусом: $bonus")
+//    manager.add(minerals)
+//    manager.add(gas)
+//    manager.printAll()
+//
+//    val generator = EnergyGenerator()
+//    val lab = ResearchLab()
+//
+//    val generatorResult = generator.performAction(manager)
+//    val labResult = lab.performAction(manager)
+//    handleMobuleResult(generatorResult)
+//    handleMobuleResult(labResult)
+//    println()
+//    manager.printAll()
+//
+//
+//
+//    val max = InstantMessenger("Max")
+////    val photoCamera = PhotoCamera()
+//    val yotaPhone = InstantMessenger.SmartPhone("YotaPhone", max)
+//    yotaPhone.send("Hello Kotlin")
+//    yotaPhone.send("Learn delegation")
+//    yotaPhone.sendTextMessage()
+//    yotaPhone.sendVideoMessage()
+//
+//}
+//fun handleMobuleResult(result: ModuleResult) {
+//    when (result) {
+//        is ModuleResult.Sucess ->
+//            println("УСПЕХ: ${result.message}")
+//
+//        is ModuleResult.ResourceProduced ->
+//            println("Произведено: ${result.resourceName} +${result.amount}")
+//
+//        is ModuleResult.NotEnoughResources ->
+//            println(
+//                "Недостаточно ресурса ${result.resourceName}. " +
+//                        "Нужно: ${result.required}, есть: ${result.available}"
+//            )
+//
+//        is ModuleResult.Error ->
+//            println("ОШИБКА: ${result.reason}")
+//    }
+//}
 fun main(){
-    val manager = ResourceManager()
-    val minerals = OutpostResource(1,"Minerals",300)
-    val gas = OutpostResource(2,"Gas",100)
-    val bonus = minerals.copy(amount = minerals.amount + 50)
-    println("Копия минералов с бонусом: $bonus")
-    manager.add(minerals)
-    manager.add(gas)
-    manager.printAll()
-
-    val generator = EnergyGenerator()
-    val lab = ResearchLab()
-
-    val generatorResult = generator.performAction(manager)
-    val labResult = lab.performAction(manager)
-    handleMobuleResult(generatorResult)
-    handleMobuleResult(labResult)
-    println()
-    manager.printAll()
+    counter = 1
+    counter = 2
 }
-fun handleMobuleResult(result: ModuleResult) {
-    when (result) {
-        is ModuleResult.Sucess ->
-            println("УСПЕХ: ${result.message}")
-
-        is ModuleResult.ResourceProduced ->
-            println("Произведено: ${result.resourceName} +${result.amount}")
-
-        is ModuleResult.NotEnoughResources ->
-            println(
-                "Недостаточно ресурса ${result.resourceName}. " +
-                        "Нужно: ${result.required}, есть: ${result.available}"
-            )
-
-        is ModuleResult.Error ->
-            println("ОШИБКА: ${result.reason}")
-    }
-}
-
--- src/gameCharacter/Main.kt
+++ src/gameCharacter/Main.kt
@@ -1,21 +1,26 @@
-package gameCharacter
-
-fun handleState(state: CharacterState){
-    when(state){
-        is CharacterState.Idle -> println("Персанаж бездействует")
-        is CharacterState.Running -> println("Персанаж бежит")
-        is CharacterState.Attacking -> println("Персанаж атакует с уроном ${state.damage}")
-        is CharacterState.Dead -> println("Персанаж погиб: ${state.cause}")
-    }
-}
-fun main(){
-    val character = GameCharacter("Герой")
-    character.changeState(CharacterState.Running)
-    handleState(character.state)
-
-    character.changeState(CharacterState.Attacking(42))
-    handleState(character.state)
-
-    character.changeState(CharacterState.Dead("Убит драконом"))
-    handleState(character.state)
-}
+//package gameCharacter
+//
+//import example.InstantMessenger
+//import example.PhotoCamera
+//
+//fun handleState(state: CharacterState){
+//    when(state){
+//        is CharacterState.Idle -> println("Персанаж бездействует")
+//        is CharacterState.Running -> println("Персанаж бежит")
+//        is CharacterState.Attacking -> println("Персанаж атакует с уроном ${state.damage}")
+//        is CharacterState.Dead -> println("Персанаж погиб: ${state.cause}")
+//    }
+//}
+//fun main(){
+//    val character = GameCharacter("Герой")
+//    character.changeState(CharacterState.Running)
+//    handleState(character.state)
+//
+//    character.changeState(CharacterState.Attacking(42))
+//    handleState(character.state)
+//
+//    character.changeState(CharacterState.Dead("Убит драконом"))
+//    handleState(character.state)
+//
+//
+//}
